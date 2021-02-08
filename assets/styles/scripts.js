c09ec2109416797c67b0c411e5e0428545b4ffcd
Jchenhall <chenhall_j@hotmail.co.uk>
2021-02-08 08:54:00 +0000

    monday showcase

---
+++ assets/styles/scripts.js
@@ -0,0 +1,73 @@
+//nav bar hamburger toggle
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarlinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+   navbarlinks.classList.toggle('active')
+})
+
+
+
+//contactform.
+function deleteMessages() {        
+   if (window.sessionStorage.getItem("messages")) {
+       window.sessionStorage.removeItem("messages");
+   }
+   
+                renderMessages();
+               }
+   
+              function submitMessage(event) {
+                  event.preventDefault();
+                  const email = document.getElementById("email").value;
+                  const fullname = document.getElementById("fullname").value;
+                  const comment = document.getElementById("comment").value;
+   
+                  const messageObject = {
+                      email, fullname, comment
+                  };
+   
+                  
+   
+               let currentMessages = [];
+   
+               if (window.sessionStorage.getItem("messages")) {
+                   currentMessages = 
+                   JSON.parse(
+                       window.sessionStorage.getItem("messages")
+                   );
+               }
+   
+               currentMessages.push(messageObject);
+               window.sessionStorage.setItem(
+                   "messages",
+                   JSON.stringify(currentMessages)
+               );
+   
+              } 
+   
+              function renderMessages() {
+               let currentMessages = [];
+   
+   if (window.sessionStorage.getItem("messages")) {
+       currentMessages = 
+       JSON.parse(
+           window.sessionStorage.getItem("messages")
+       );
+   }    
+   
+   let listItems = [];
+   for (let i = 0; i < currentMessages.length; i++) {
+       let listItem = "";
+       const currentMessage = currentMessages[i];
+       listItem += `<dt>${currentMessage.fullname} - ${currentMessage.email}</dt>`;
+       listItem += `<dd>${currentMessage.comment}</dd>`;
+           listItem += `<br />`;
+           listItems.push(listItem);
+   }
+   
+   let descList = document.getElementById('currentMessages');
+   descList.innerHTML = listItems.join('');
+              }
+   
+              renderMessages();
